c882f675e9ecaa7224e7c195277d08ad84a9bd50
machida <machida@kmc.gr.jp>
2019-08-26 22:38:26 +0900

    Room, Chat_messageモデル・コントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'rooms#index'
+  get 'rooms/index'
+  get 'rooms/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
