bd705ffba13626b4a2cd9a9d4e533186d1811a02
machida <machida@kmc.gr.jp>
2019-08-27 19:11:26 +0900

    channel 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+
   root 'rooms#index'
   get 'rooms/index'
   get 'rooms/show/:id', to: 'rooms#show', as: 'room'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
